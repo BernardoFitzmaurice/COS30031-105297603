2300d1bdc9f5ba1f7bb61028d5db02f53be8614f
Bernardo Fitzmaurice <105297603@student.swin.edu.au>
2024-09-19 20:49:51 +1000

    Part 1 Finished
    Finished part 1 code and questions

--- 09c - Lab - File Input Output/Lab09/Lab09.cpp
+++ 09c - Lab - File Input Output/Lab09/Lab09.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 class compoundType {
@@ -14,15 +15,65 @@ int main()
     compoundType Goku;
 	Goku.frog = 'E';
 	Goku.power = 9000;
-	Goku.tacos = 2;
+	Goku.tacos = 6.7;
 
+	/*
 	//Writing values to text file
-	//ofstream outFile("");
+	ofstream outFile("GokuData.txt");
+	if (outFile.is_open()) {
+		outFile << Goku.frog << endl;
+		outFile << Goku.power << endl;
+		outFile << Goku.tacos << endl;
+		outFile.close();
+	} else {
+		std::cout << "Error opening file" << endl;
+	}*/
+
+	/*
+	//Reading values from text file
+	compoundType GokuFromFile;
+	ifstream inFile("GokuData.txt"); 
+	if (inFile.is_open()) {
+		inFile >> GokuFromFile.frog;
+		inFile >> GokuFromFile.power;
+		inFile >> GokuFromFile.tacos;
+		inFile.close();
+	}
+	else {
+		std::cout << "Error opening file" << endl;
+	}
 
 	//Outpot to the console
-	std::cout << "Goku's frog is: " << Goku.frog << endl;
-	std::cout << "Goku's power level is: " << Goku.power << endl;
-	std::cout << "Goku's taco count is: " << Goku.tacos << endl;
+	cout << "Goku's frog is: " << GokuFromFile.frog << endl;
+	cout << "Goku's power level is: " << GokuFromFile.power << endl;
+	cout << "Goku's taco count is: " << GokuFromFile.tacos << endl;
+	*/
+
+	/*
+	//Values in binary file
+	ofstream binFile("test1.bin", ios::binary); //Opens binari file in write mode
+	if (binFile.is_open()) {
+		binFile.write(reinterpret_cast<char*>(&Goku), sizeof(Goku)); //Write the binary
+		binFile.close();
+	} else {
+		std::cout << "Error opening file" << endl;
+	}
+	*/
+
+	//Reading values from binary file
+	compoundType GokuFromBinary;
+	ifstream binInputFile("test1.bin", ios::in | ios::binary); //Opens binary file in read mode
+
+	if (binInputFile.is_open()) {
+		binInputFile.read(reinterpret_cast<char*>(&GokuFromBinary), sizeof(GokuFromBinary)); //Read the binary
+		binInputFile.close(); //close binary
+	} else {
+		std::cout << "Error opening file" << endl;
+	}
+
+	cout << "Goku's frog is: " << GokuFromBinary.frog << endl;
+	cout << "Goku's power level is: " << GokuFromBinary.power << endl;
+	cout << "Goku's taco count is: " << GokuFromBinary.tacos << endl;
 
 	return 0;
 }
