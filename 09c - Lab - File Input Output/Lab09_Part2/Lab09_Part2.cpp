d9cffe9d3d13bc76205fae57714e8822c8291c95
Bernardo Fitzmaurice <105297603@student.swin.edu.au>
2024-09-22 17:55:32 +1000

    Part 2
    Code for part 2 completed.

--- 09c - Lab - File Input Output/Lab09_Part2/Lab09_Part2.cpp
+++ 09c - Lab - File Input Output/Lab09_Part2/Lab09_Part2.cpp
@@ -19,7 +19,44 @@ int main()
 	std::string line;
 	while (std::getline(inputFile, line)) //Read file line by line
 	{
-		cout << line << endl;
+		//Trim whitespace
+		line.erase(0, line.find_first_not_of(" \t\n\r")); // Trim leading whitespace
+		line.erase(line.find_last_not_of(" \t\n\r") + 1); // Trim trailing whitespace
+
+		//Ignore blank lines
+		if (line.empty())
+		{
+			continue;
+		}
+
+		//Ignore lines that start with #
+		if (line[0] == '#')
+		{
+			continue;
+		}
+
+		//Split line by ":"
+		std::istringstream ss(line);
+		std::string token;
+		vector<string> tokens;
+		while (getline(ss, token, ':'))
+		{
+			tokens.push_back(token);
+		}
+
+		//Check number of tokens
+		if (tokens.size() == 3)
+		{
+			//Print token on new line
+			for (const auto& t : tokens)
+			{
+				cout << t << endl;
+			}
+		}
+		else 
+		{
+			cout << "Invalid number of tokens: " << line << endl;
+		}
 	}
 
 	inputFile.close(); //Close file
