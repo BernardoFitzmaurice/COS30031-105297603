239282ee44919967ba00c233c08cfe627d040cb1
Bernardo Fitzmaurice <105297603@student.swin.edu.au>
2024-10-15 01:30:02 +1100

    Spike 12 update
    Simple modifications

--- 12c - Spike - Command Pattern/Task_Spike12/Task_Spike12.cpp
+++ 12c - Spike - Command Pattern/Task_Spike12/Task_Spike12.cpp
@@ -11,6 +11,7 @@ struct Location
 {
     string name;
     string description;
+    vector<string> entities; // List of entities in the location
     map<string, string> connections; // direction -> location name
 };
 
@@ -20,6 +21,64 @@ string lowercase(const string& str) {
         c = tolower(c);
     }
     return result;
+}
+
+// Process user command
+void processCommand(const string& command, const string& argument, map<string, Location>& gameWorld, string& currentLocation)
+{
+	// Command to move in specified direction
+    if (command == "go")
+    {
+        string direction = lowercase(argument);
+        Location& loc = gameWorld[currentLocation];
+        if (loc.connections.find(direction) != loc.connections.end())
+        {
+            currentLocation = loc.connections[direction];
+        }
+        else
+        {
+            cout << "You crashed to a wall like a frog.\n" << endl;
+        }
+    }
+    // COmmand of Location details
+    else if (command == "look")
+    {
+        Location& loc = gameWorld[currentLocation];
+        cout << "\nYou are in: " << loc.name << endl;
+        cout << loc.description << endl;
+        cout << "Entities here: ";
+        for (const auto& entity : loc.entities)
+        {
+            cout << entity << ", ";
+        }
+        cout << endl;
+    }
+	// Command of Entitie details
+    else if (command == "look at")
+    {
+        Location& loc = gameWorld[currentLocation];
+        if (find(loc.entities.begin(), loc.entities.end(), argument) != loc.entities.end())
+        {
+            cout << "You see a " << argument << " here.\n";
+        }
+        else
+        {
+            cout << "No such entity here.\n";
+        }
+    }
+    // To show all commands
+    else if (command == "help")
+    {
+        cout << "Available commands:\n";
+        cout << "go <direction> - Move to a different location.\n";
+        cout << "look - Show details of the current location.\n";
+        cout << "look at <entity> - Show details of a specific entity.\n";
+        cout << "help - List known commands.\n";
+        cout << "inventory - Show what the player has.\n";
+        cout << "alias <alias> <command> - Remap a command.\n";
+        cout << "debug tree - Show debug information.\n";
+    }
+    // Additional commands like INVENTORY, ALIAS, and DEBUG TREE can be implemented similarly.
 }
 
 int main(int argc, char* argv[])
@@ -76,7 +135,22 @@ int main(int argc, char* argv[])
 
                 // Store the connection
                 loc.connections[direction] = destLocation;
-                cout << "Loading" << locationName << "direction: " << direction << " -> " << destLocation << '\n';
+                cout << "Loading " << locationName << " direction: " << direction << " -> " << destLocation << '\n';
+            }
+
+            // Read the entities
+            getline(inputFile, line);
+            if (line.find("Entities") != string::npos)
+            {
+                string entitiesStr = line.substr(line.find(":") + 2);
+                stringstream entitiesStream(entitiesStr);
+                string entity;
+                while (getline(entitiesStream, entity, ','))
+                {
+                    entity.erase(entity.find_last_not_of(' ') + 1); // Remove trailing spaces
+                    entity.erase(0, entity.find_first_not_of(' ')); // Remove leading spaces
+                    loc.entities.push_back(entity);
+                }
             }
 
             // Add the location to the game
@@ -87,6 +161,7 @@ int main(int argc, char* argv[])
     inputFile.close();
 
     // 'Go' & 'Quit' commands
+    map<string, string> aliases; // To store command aliases
     string currentLocation = "Kitchen"; // Starting location, no lowercase conversion
     string input;
 
@@ -115,23 +190,16 @@ int main(int argc, char* argv[])
             cout << "Weak" << endl;
             break;
         }
-        else if (input.substr(0, 3) == "go ")
+        size_t spacePos = input.find(' ');
+        string command = input.substr(0, spacePos);
+        string argument = (spacePos != string::npos) ? input.substr(spacePos + 1) : "";
+
+        if (aliases.find(command) != aliases.end())
         {
-            string direction = input.substr(3);
-            direction = lowercase(direction); // convert to lowercase
-            if (loc.connections.find(direction) != loc.connections.end())
-            {
-                currentLocation = loc.connections[direction]; // Move to new location
-            }
-            else
-            {
-                cout << "You crashed to a wall like a frog.\n" << endl;
-            }
+            command = aliases[command]; // Use alias if it exists
         }
-        else
-        {
-            cout << "Invalid command.\n " << endl;
-        }
+
+        processCommand(command, argument, gameWorld, currentLocation);
     }
 
     return 0;
